2bbfc7dd487f4c8758c2ea7a94e40acd2ef6af59
Michał Januszewski <spock@gentoo.org>
2006-07-14 17:34:01 +0200

    Added a simple 'configure' script.

--- core/config.h
+++
@@ -1,7 +0,0 @@
-#define CONFIG_PNG
-#define CONFIG_TTF
-#define CONFIG_TTF_KERNEL
-#define CONFIG_FBSPLASH
-#define CONFIG_MNG
-#define THEME_DIR 			"/etc/splash"
-#define SPLASH_FIFO			"/lib/splash/cache/.splash"
