30c4e0fa00cca0ef2e68addafa0b3e55caa0e644
Michał Januszewski <spock@gentoo.org>
2007-04-05 12:35:35 +0200

    Restore bytespp calculation that was accidentally removed.

--- core/common.c
+++ core/common.c
@@ -82,6 +82,8 @@ int get_fb_settings(int fb_num)
 	}
 	close(fb);
 
+	bytespp = (fb_var.bits_per_pixel + 7) >> 3;
+
 	/* Check if optimized code can be used. We use special optimizations for
 	 * 24/32bpp modes in which all color components have a length of 8 bits. */
 	if (bytespp < 3 || fb_var.blue.length != 8 || fb_var.green.length != 8 || fb_var.red.length != 8) {
